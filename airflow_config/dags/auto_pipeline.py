b6f6bf14e2c2138c358724648eee4b985fff0d5b
IrinaEkb <irinafendley@gmail.com>
2026-08-11 10:51:36 -0400

    ETL fix

--- airflow_config/dags/auto_pipeline.py
+++ airflow_config/dags/auto_pipeline.py
@@ -1,15 +1,13 @@
 import sys
 from pathlib import Path
-
-from src.load.load_postgres import run_load
+from datetime import datetime
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
 
 from airflow.sdk import dag, task
-
-from datetime import datetime
 
 
 @dag(
@@ -26,47 +24,43 @@ from datetime import datetime
 )
 def xbrl_financial_pipeline():
 
-
     @task
     def extract():
-
         from src.extract.extract_sec import run_extract
 
-        result = run_extract()
-
-        print(f"Extract completed: {result}")
-
-        return str(result)
-
+        output_file = run_extract()
+        print(f"Extract completed: {output_file}")
+        return str(output_file)
 
     @task
-    def transform():
+    def generate_mapping(raw_file):
+        from utils.generate_mapping import generate_mapping as run_generate_mapping
 
-        from src.transform.transform_financials import transform_financials
+        mapping_file = run_generate_mapping(raw_file)
+        print(f"Mapping completed: {mapping_file}")
+        return str(mapping_file)
 
-        result = transform_financials()
+    @task
+    def transform(input_file):
+        from src.transform.transform_financials import run_transform
 
-        print("Transform completed")
-
-        return result
+        output_file = run_transform(input_file)
+        print(f"Transform completed: {output_file}")
+        return str(output_file)
 
     @task
     def load(file):
-
         from src.load.load_postgres import run_load
 
         run_load(file)
-
         print("Load completed")
 
-
     extract_task = extract()
-    transform_task = transform()
+    mapping_task = generate_mapping(extract_task)
+    transform_task = transform(extract_task)
     load_task = load(transform_task)
 
-
-    extract_task >> transform_task >> load_task 
-
+    extract_task >> mapping_task >> transform_task >> load_task
 
 
 xbrl_financial_pipeline()
